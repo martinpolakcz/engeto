1d99824437b4b04e99a56f4ec8ba12af2a17635a
Martin Polak <m.polak69@gmail.com>
2024-02-14 21:45:51 +0100

    OPRAVA 14.02.2024

--- Projekt_martin_polak_2/martin_polak_projekt2.py
+++ Projekt_martin_polak_2/martin_polak_projekt2.py
@@ -15,78 +15,65 @@ if os.name == 'nt':
 else:  
    os.system('clear')
 
+
+lenght_pass = 4
+
 #generovani nahodneho cisla s kontrolou na opakovani cisel
 def generate_random_number():
-    unique = False
-    previously_generated = set()
-    while not unique:
-        num = random.randint(1000, 9999)
-        num_duplicity = 0
-        if len(previously_generated) == 0 or num not in previously_generated:
-            previously_generated.add(num)
-            check=str(num)
-            for i in range(len(check)):
-               for x in range(len(check)):
-                  if i==x:
-                     continue
-                  elif check[i]==check[x]:
-                      num_duplicity = 1
-                      break
-        if num_duplicity == 0 and previously_generated != num:
-           previously_generated.add(num)
-           unique =  True
-        else:
-           pass
+   secret_number=[]
+   num_list = [0,1,2,3,4,5,6,7,8,9]
+   num_0 = random.sample(num_list[1:], 1)
+   integer_value = int(str(num_0).strip('[]').replace(',', ''))
+   secret_number.append(integer_value)
+   num_list.remove(integer_value)
 
-    return num
+   num=dict()
+   for i in  range(lenght_pass - 1):
+      num[i] = random.sample(num_list, 1)
+      integer_value = int(str(num[i]).strip('[]').replace(',', ''))
+      secret_number.append(integer_value)
+      num_list.remove(integer_value)
+      
+   secret_number = int(''.join(map(str, secret_number)))
+   return str(secret_number)
 
 #kontrola samotne hry
 def check_tip(tip_user,generated_number):
-   tip_user = list(map(int, str(tip_user)))
-   generated_number = list(map(int, str(generated_number)))
+
    bulls = 0
    cows = 0
    for i in range(len(tip_user)):
-    if (tip_user[i] == generated_number[i]) and (tip_user[i] in generated_number):
+    if tip_user[i] == generated_number[i]:
        # bulls pokud je cislo na stejnem miste a nebo je obsazen v cisle
        bulls += 1
-       cows += 1
-
-    elif (tip_user[i] != generated_number[i]) and (tip_user[i] in generated_number):
+       
+    if  tip_user[i] in generated_number:
         # cows pokud je cislo aspon nekde v cisle na nejake pozici
        cows += 1
-    continue
+    
 
             
-   print(f'Bulls:{bulls}')
-   print(f'Cows:{cows}') 
-   return bulls    
+
+   return bulls  ,cows  
 
 #kontrola validace hadaneho cisla
-def validation_number():
-   validation_ok = False
-   while validation_ok == False:
-      choose_number= input("Enter a number: ")
-      list_number = list( str(choose_number))
-      if not (choose_number.isdigit()):
+def validation_number(check_input):
+      
+      list_number = list( str(check_input))
+      if not (check_input.isdigit()):
          print("Your input doesn't include only digits")
-         validation_ok = False
-      elif len(choose_number) <4 : 
-       print(f'Your number {choose_number} is less digits than 4')
-       validation_ok = False
+      elif len(check_input) <4 : 
+       print(f'Your number {check_input} is less digits than 4')
       elif(list_number[0]=='0'):
          print('Number start with 0 digits, which is incorrect')
-         validation_ok = False
-      elif(len(choose_number) >4):
+      elif(len(check_input) >4):
          print('Number can not be longer than 4 digits')
-         validation_ok = False
       else:
          print("Your input is correct, welcome in the game")
          print(pomlcky*20)
-         validation_ok = True
-         return choose_number
+         return True
 
-#zacatek programu
+#zacatek programu Let's play
 pomlcky = '-'
 print("Hi there!")
 print(pomlcky*20)
@@ -94,18 +81,26 @@ print("I've generated a random 4 digit number for you.")
 print("Let's play a bulls and cows game.")
 print(pomlcky*20)
 
+#generovani nahodneho cisla
 random_number = generate_random_number()
-#print(random_number)  --jen pro testovani
+#print(f'Generated number is: {random_number}')  #--jen pro testovani
 
-value=0
+bulls=0
 attempts =0
+#spusteni casovace pro "competition"
 start_time = time.time()
-while value !=4:   
-   choose_number=validation_number()
+while bulls !=4:  
+   choose_number= input("Enter a number: ") 
+   #validace vstupu pomoci funkce
+   while  not validation_number(choose_number):
+     choose_number= input("Please enter again a correct number: ")
    print(f'>>> {choose_number}')
-   
-   value=check_tip(choose_number,random_number)
-   if value != 4:
+   #kontrola TIPu
+   bulls ,cows =check_tip(choose_number,random_number)
+   print(f'Bulls:{bulls}')
+   print(f'Cows:{cows}') 
+  # value = bulls
+   if bulls != 4:
       print("Try again")
       attempts +=1
       print(pomlcky*20)
@@ -114,3 +109,4 @@ while value !=4:
       elapsed_time = round(end_time - start_time,2)
       print("You won! Congratulations!")
       print(f"Number of attempts: {attempts} and in time {elapsed_time} seconds")
+
